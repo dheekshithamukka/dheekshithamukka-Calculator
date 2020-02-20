123bd4f483f3cf1d876aec6aaa28d1b78fec3651
Mukka Dheekshitha <41354527+dheekshithamukka@users.noreply.github.com>
2020-02-20 23:16:58 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+package com.epam.design_calculator;
+
+public class Calculator {
+	public float add(float n1, float n2) {
+		return n1 + n2;
+	}
+	
+	public float subtract(float n1, float n2) {
+		return n1-n2;
+	}
+	
+	public float multiply(float n1, float n2) {
+		return n1*n2;
+	}
+	
+	public float divide(float n1, float n2) {
+		return n1/n2;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.epam.design_calculator;
+
+public class Main {
+	public static void main(String[] args) {
+		   Output.presentation();
+		   
+		}
+
+}
